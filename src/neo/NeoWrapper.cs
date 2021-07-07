d0fd8377820dcc344ec0e0a04987fcb4eb4cdcde
晨 黎 <lichen@neo.org>
2021-07-07 17:51:24 +0800

    add fee condition

--- src/neo/NeoWrapper.cs
+++ src/neo/NeoWrapper.cs
@@ -163,7 +163,7 @@ namespace neo_wrapper
             _assert(toChainId != NeoChainId && toChainId != 0, "!toChainId");
             _assert(amount > fee, "amount less than fee");
             _assert(!paused(), "paused");
-            _pull(fromAsset, fromAddress, fee);
+            if(fee != 0) _pull(fromAsset, fromAddress, fee);
             _push(fromAsset, fromAddress, toChainId, toAddress, amount - fee);
             PolyWrapperLock(fromAsset, fromAddress, toChainId, toAddress, amount - fee, fee, id);
             return true;
